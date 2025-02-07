7c496d8eb0d76a93a0d37b429d5190e703cee6f4
epct <epct.programming@gmail.com>
2025-02-07 17:20:45 +0100

    Initial commit

---
+++ src/main/java/net/epct/skelethrone/SkelehroneClient.java
@@ -0,0 +1,10 @@
+package net.epct.skelethrone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkelehroneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
