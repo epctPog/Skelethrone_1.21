7806c593f7fdd342db33df0fadcbf934e88fe0ed
epct <epct.programming@gmail.com>
2025-02-08 23:44:52 +0100

    Skelethrone_1.02(Discovery-phase)

--- src/main/java/net/epct/skelethrone/block/ModBlocks.java
+++ src/main/java/net/epct/skelethrone/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.epct.skelethrone.Skelethrone;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,15 +12,23 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
-            new Block(AbstractBlock.Settings.create().strength(0.5f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+            new Block(AbstractBlock.Settings.create().strength(2.5f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
-            new Block(AbstractBlock.Settings.create().strength(0.25f).requiresTool().sounds(BlockSoundGroup.NETHERRACK)));
+            new Block(AbstractBlock.Settings.create().strength(3.5f).requiresTool().sounds(BlockSoundGroup.NETHERRACK)));
     public static final Block FOSSIL_ROCK_BLOCK = registerBlock("fossil_rock_block",
-            new Block(AbstractBlock.Settings.create().strength(0.2f).requiresTool().sounds(BlockSoundGroup.STONE)));
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 10),
+                    AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.STONE)));
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(2f).requiresTool().sounds(BlockSoundGroup.STONE)));
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 7),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -43,6 +52,8 @@ public class ModBlocks {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
             entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
             entries.add(ModBlocks.FOSSIL_ROCK_BLOCK);
+            entries.add(ModBlocks.PINK_GARNET_ORE);
+            entries.add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
         });
     }
 }
